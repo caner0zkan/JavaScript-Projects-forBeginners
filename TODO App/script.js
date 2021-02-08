e605cfc03c0458a8f4d092434665819ede91f83f
M. Caner Özkan <canerozkan513@gmail.com>
2021-02-08 23:07:32 +0300

    n

--- TODO App/script.js
+++ TODO App/script.js
@@ -1,38 +1,34 @@
-//ADD
 const todoAdd = document.getElementById("buttonAdd");
+const todoDelete1 = document.getElementsByClassName("buttonDelete");
 
+//ADD
 todoAdd.addEventListener("click", () => {
-    console.log("clicked");
+  var p = document.createElement("p");
+  var inputValue = document.getElementById("input").value;
+  var t = document.createTextNode(inputValue);
+  p.appendChild(t);
+  p.setAttribute("class" , "mission");
 
-    var p = document.createElement("p");
-    var inputValue = document.getElementById("input").value;
-    var t = document.createTextNode(inputValue);
-    p.appendChild(t);
-    p.setAttribute("class" , "mission");
-    var html = `${inputValue} <button onclick="todoDelete()" class="buttonDelete"><i class="fas fa-trash trash"></i>
-    `;
-    p.innerHTML = html;
+  var html = `${inputValue} <button onclick="todoDelete()" class="buttonDelete"><i class="fas fa-trash trash"></i>`;
+  p.innerHTML = html;
 
-    if(inputValue == ""){
-        alert("You must write something!");
-    }
-    else{
-        document.getElementById("todoMissions").appendChild(p);
-    }
-    document.getElementById("input").value = "";
+  if(inputValue == ""){
+      alert("You must write something!");
+  }
+  else{
+      document.getElementById("todoMissions").appendChild(p);
+  }
+  document.getElementById("input").value = "";
 });
 
 
 //DELETE
-
 function todoDelete(){
-    var close = document.getElementsByClassName("buttonDelete");
-    var i;
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function(){
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
+  var close = document.getElementsByClassName("buttonDelete");
+  for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function(){
+      var div = this.parentElement;
+      div.remove("mission");
     }
+  }
 }
-
